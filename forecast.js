e70f49ae64ff3438177a8ce052219cc3045361be
tkern2 <94367743+tkern2@users.noreply.github.com>
2022-05-15 13:06:48 -0400

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -4,7 +4,7 @@ const key = '35p2eNQH7jEoo4w9F2y9if43KUgiLIuM';
 
 const getWeather = async id => {
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
@@ -17,7 +17,7 @@ const getWeather = async id => {
 
 const getCity = async city => {
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
